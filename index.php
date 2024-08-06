b3c32f500c847eaeb11916a99164c42d65118346
Musisipi WuzHere <musisipii@outlook.com>
2024-08-06 12:15:36 +0700

    fitur

--- index.php
+++ index.php
@@ -4,28 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eksperimen</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css?v2">
 </head>
 <body>
-    <nav>
-        
-        <a href="index.html">Home</a> 
-        <a href="contact.php">Project</a> 
-        <a href="about.html">About me</a>
-        <div class="logo">NurFakhri</div>
-        
-    </nav>
-
-    <header>
-    <h1>Eksperiment</h1>
-    <p style="text-align: center;">laman untuk eksperimen pembelajaran di bawah</p>
-    </header>
-
-
-    
-    
+   
     <main>
-    <hr> 
+  
     <br><br>
     <center>
       <h1> TABEL </h1>
@@ -44,29 +28,32 @@
 {
 echo '<tr>
 
-     
+ 
 <td>'.$row['no'].' </td>
 <td>'.$row['nama_siswa'].'</td>
 <td>'.$row['kelas'].'</td>
 <td><img src="image.png" style="width:20px"></td>
 
-</tr>';
-}      
+</tr> ';
+}
+  ?>
+      
         
-      ?>
+      
     
       
       </table>
-
+     
       
       <br>
-      <button> <a href="login.html"> > </a></button>
-      <blockquote style="font-family: 'Courier New', Courier, monospace;"> Tabel By NurFakhri </blockquote>
-      <br> <br> <br>
       
-    </main>
+      <button class="kliklogin" role="button"> <a href="login.html"> LOGIN </a></button>
+     
+      <blockquote> Tabel By NurFakhri </blockquote>
+      <br> <br> <br>
+      </main>
+ 
 
-  
    
     <footer style="text-align: center; margin-top: auto;">   
     <p> Create By NurFakhri</p>
